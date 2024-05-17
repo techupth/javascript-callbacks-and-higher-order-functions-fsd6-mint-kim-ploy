c97a7f6165c1417f7dfcfc8f530bc65df65e6b92
PNKim <paljapon.nait@gmail.com>
2024-05-17 11:48:32 +0700

    feat: resolve ex1-2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,21 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i], newArray);
+  }
+  if (newArray.length > 4) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
+
+function rommPass(score, newArray) {
+  if (score > 70) {
+    newArray.push(score);
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +25,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, rommPass);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, rommPass);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, rommPass);
+
+console.log("นักเรียนห้องที่ 1 " + scoreRoom1Result);
+console.log("นักเรียนห้องที่ 2 " + scoreRoom2Result);
+console.log("นักเรียนห้องที่ 3 " + scoreRoom3Result);
