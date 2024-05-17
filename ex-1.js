87efbaa32fd663bbc33209b6f52b3195a64daedb
nattawara <nattawara.jtk@gmail.com>
2024-05-17 14:30:15 +0700

    ploy/high order and callback function assignment

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,20 @@
 
 function forEach(array, operation) {
   // Start coding here
+  return operation(array);
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
+
+const add = function (array) {
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    newEmployeeSalaries.push(array[i] + 5000);
+  }
+};
 
 // Using `forEach` function here
+forEach(employeeSalaries, add);
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries);
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,38 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
+  return operation(array);
   // Start coding here
 }
+let roomNumber = 0;
+const testResult = function (array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      result += 1;
+    } else {
+      result = result;
+    }
+  }
+  if (result >= 5) {
+    result = "ผ่านเกณฑ์ ✅";
+    roomNumber += 1;
+  } else {
+    result = "ไม่ผ่านเกณฑ์ ❌";
+    roomNumber += 1;
+  }
+  return `นักเรียนห้องที่ ${roomNumber} ${result}`;
+};
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
+//console.log(Object.keys({ studentScoresRoom3 })[0].charAt(17));
 // Using `atLeastFive` function here
-
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, testResult);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, testResult);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, testResult);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
